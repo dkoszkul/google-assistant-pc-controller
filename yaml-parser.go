8b96f2916fbdd9afb9882219d9e42446ab86bdd4
Dominik <dominik.koszkul@gmail.com>
2018-10-26 12:23:04 +0200

    Create yaml-parser.go

---
+++ yaml-parser.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+        "log"
+        "fmt"
+        "io/ioutil"
+        "gopkg.in/yaml.v2"
+)
+
+type Command struct {
+        Cmd string `yaml:"cmd"`
+        Key string `yaml:"key"`
+}
+
+type Commands struct {
+        Cmds []Command `yaml:"commands"`
+}
+
+func main() {
+        log.Println("Start application")
+        data, err := ioutil.ReadFile("file.yaml")
+        CheckError(err)
+
+        commands := Commands{}
+        err = yaml.Unmarshal([]byte(data), &commands)
+        CheckError(err)
+
+        c := commands.Cmds
+        fmt.Println(c)
+
+        cMap := map[string]string{}
+        for _, v := range c {
+                cMap[v.Key] = v.Cmd
+        }
+
+        fmt.Println(cMap)
+
+}
+
+func CheckError(e error) {
+        if e != nil {
+                log.Fatalf("error: %v", e)
+        }
+}
